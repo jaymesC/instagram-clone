0508b53364a7521ed8465838c7526b7c52a919a4
James <clementjaymes@.com>
2021-08-25 20:21:16 -0700

    adding firebase config files

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
-// const firebaseApp = firebase.initializeApp({
-//   apiKey: "AIzaSyCBf80D3anyLTXrlS5jSicQs0AR0sfLzcs",
-//   authDomain: "instagram-clone-2ca9c.firebaseapp.com",
-//   projectId: "instagram-clone-2ca9c",
-//   storageBucket: "instagram-clone-2ca9c.appspot.com",
-//   messagingSenderId: "992271874579",
-//   appId: "1:992271874579:web:292c28ee88efd140b0dde6",
-//   measurementId: "G-1N9JYK2YZZ",
-// });
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCBf80D3anyLTXrlS5jSicQs0AR0sfLzcs",
+  authDomain: "instagram-clone-2ca9c.firebaseapp.com",
+  projectId: "instagram-clone-2ca9c",
+  storageBucket: "instagram-clone-2ca9c.appspot.com",
+  messagingSenderId: "992271874579",
+  appId: "1:992271874579:web:292c28ee88efd140b0dde6",
+  measurementId: "G-1N9JYK2YZZ",
+});
 
 // const db = firebaseApp.firestore();
 // const auth = firebase.auth();
